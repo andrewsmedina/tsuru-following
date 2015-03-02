19211e63d9b58250107cffde10b814e25d0493c7
Andrews Medina <andrewsmedina@gmail.com>
2015-03-02 18:01:09 -0300

    add app list.

--- app.py
+++ app.py
@@ -1,14 +1,25 @@
 from flask import Flask
+from flask import render_template
 
 import os
 
+import requests
+
 
 app = Flask(__name__)
+app.debug = os.environ.get('DEBUG', '0') in ('true', 'True', '1')
+
+
+TSURU_TOKEN = os.environ.get("TSURU_TOKEN")
+TSURU_HOST = os.environ.get("TSURU_HOST")
 
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def app_list():
+    headers = {'authorization': TSURU_TOKEN}
+    url = '{}/apps'.format(TSURU_HOST)
+    app_list = requests.get(url, headers=headers).json()
+    return render_template('app_list.html', app_list=app_list)
 
 
 if __name__ == "__main__":
